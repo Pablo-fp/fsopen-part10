520d1e4fbc46dd4e87afb81d49f18a15761f4af0
Pablo-fp <pablofp1988@hotmail.com>
2025-06-08 17:19:49 +0200

    Enhancing Apollo Client's requests

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import AuthStorage from './src/utils/authStorage';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,9 +1,35 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Constants from 'expo-constants';
+import { setContext } from '@apollo/client/link/context';
 
-const createApolloClient = () => {
+const httpLink = createHttpLink({
+  uri: Constants.expoConfig.extra.apolloUri
+});
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      // 1. Get the authentication token from storage
+      const accessToken = await authStorage.getAccessToken();
+
+      // 2. Return the headers to the context so httpLink can read them
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : ''
+        }
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers
+      };
+    }
+  });
+
   return new ApolloClient({
-    uri: Constants.expoConfig.extra.apolloUri,
+    // 3. Chain the authLink and httpLink together
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
   });
 };

fbbbd0772c43f1410fda700d34b982017f6c5b30
Pablo-fp <pablofp1988@hotmail.com>
2025-06-08 20:20:37 +0200

    Exercise 10.4 and Using React Context for dependency injection

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,13 +1,20 @@
 import { useMutation } from '@apollo/client';
 import { AUTHENTICATE } from '../graphql/mutations';
+import useAuthStorage from '../hooks/useAuthStorage';
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTHENTICATE);
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
       variables: { credentials: { username, password } }
     });
+
+    if (data?.authenticate?.accessToken) {
+      await authStorage.setAccessToken(data.authenticate.accessToken);
+    }
+
     return { data };
   };
 

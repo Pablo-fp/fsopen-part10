7b3064e9e636e389eba4dfba83ffff7633498d44
Pablo-fp <pablofp1988@hotmail.com>
2025-06-09 21:48:23 +0200

    Set Testing React Native applications

--- rate-repository-app/eslint.config.js
+++ rate-repository-app/eslint.config.js
@@ -14,19 +14,24 @@ export default [
         // Add browser globals
         window: 'readonly',
         document: 'readonly',
-        navigator: 'readonly',
+        navigator: 'readonly'
       },
       parserOptions: {
-        ecmaFeatures: { jsx: true },
-      },
+        ecmaFeatures: { jsx: true }
+      }
     },
     plugins: {
       react: reactPlugin,
-      'react-native': reactNativePlugin,
+      'react-native': reactNativePlugin
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:jest/recommended'
+    ],
     rules: {
       'no-unused-vars': 'warn',
-      'react/prop-types': 'off',
-    },
-  },
+      'react/prop-types': 'off'
+    }
+  }
 ];
